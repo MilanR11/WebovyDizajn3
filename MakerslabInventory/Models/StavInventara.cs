1c3b7830b2f494c14122fe3c538dc299049961ac
MilanR11 <milanrabcan@gmail.com>
2025-11-24 17:07:30 +0100

    Uprava stav inventára

--- MakerslabInventory/Models/StavInventara.cs
+++ MakerslabInventory/Models/StavInventara.cs
@@ -5,6 +5,7 @@
         // Tieto hodnoty sa v databáze uložia ako čísla (0, 1, 2)
         Dostupný = 0,
         Vypožičaný = 1,
-        Pokazený = 2
+        Pokazený = 2,
+        NizkaZasoba = 3
     }
 }
